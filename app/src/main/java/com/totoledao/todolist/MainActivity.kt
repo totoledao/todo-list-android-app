712d64da83266f6cd4bccee571924a4fb76c97a7
Guilherme Toledo <totoledao@hotmail.com>
2022-11-24 09:21:07 -0300

    First commit

---
+++ app/src/main/java/com/totoledao/todolist/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.totoledao.todolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
